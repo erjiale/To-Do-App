665b520cbf74b1d81281470b63109e8cc65fc372
erjiale <jiale98chinoguay@gmail.com>
2020-02-11 16:42:32 -0500

    Express to Serve to Static HTML

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/To-Do-App.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(3003);
+app.listen(3000);
