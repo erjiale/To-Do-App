62c1c350cfdaa109c09c5409932bf1d9684d8326
erjiale <jiale98chinoguay@gmail.com>
2020-02-12 13:16:36 -0500

    created clunky static HTML

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var express = require('express');
-var app = express();
 var path = require('path');
+var fs = require('fs');
+
+var app = express();
+
+var data = fs.readFileSync(path.join(__dirname, '/data.json'));
+var jsondata = JSON.parse(data);
+
+for (var index in jsondata) {
+    console.log(jsondata[index].name);
+}
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// var datafile = path.join(__dirname, './data.json')
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
+    
 });
 
 app.listen(3000);
