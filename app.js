73e69d9dcb2f63700e0f587511be10721cdd2efb
erjiale <jiale98chinoguay@gmail.com>
2020-02-12 21:52:10 -0500

    completed part 1c, displayed json data as a list in HTML

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-var express = require('express');
-var path = require('path');
-var fs = require('fs');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const fs = require('fs');
 
-var app = express();
+const app = express();
+
+// these 2 lines of code configure express to use handlebars library as the view engine for the app
+app.engine("handlebars",exphbs());
+app.set("view engine", "handlebars");
 
 var data = fs.readFileSync(path.join(__dirname, '/data.json'));
 var jsondata = JSON.parse(data);
+console.log(jsondata);
+// for (var index in jsondata) {
+//     console.log(jsondata[index].name);
+// }
 
-for (var index in jsondata) {
-    console.log(jsondata[index].name);
-}
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-// var datafile = path.join(__dirname, './data.json')
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-    
+    // res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.render("home", {datajson: jsondata})
 });
 
 app.listen(3000);
