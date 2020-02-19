36efa0249eb5cdd6eb412edf5588b7d4faab7ea3
erjiale <jiale98chinoguay@gmail.com>
2020-02-19 08:15:53 -0500

    created a new user with the hunter-todo-api, need to authenticate

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+
 const app = express();
 
 // these 2 lines of code configure express to use handlebars library as the view engine for the app
@@ -21,9 +23,56 @@ app.get('/', function(req, res) {
     res.render("home", {datajson: jsondata})
 });
 
+// part 1d - Submit Data to Express App Using HTML Form
 app.post("/", function(req, res) {
     console.log("Data inputted: " + req.body.data_input);    
     res.render("home");
 });
+
+// part 1e - Use Node to Make HTTP Requests to the Hunter-to-do-app API
+TODO_API_URL = "https://hunter-todo-api.herokuapp.com";
+
+    // creates a new user with username: "jayqiu"
+app.get("/new_user", function(req,res) {
+    axios.post(TODO_API_URL + '/user', {
+        username: 'jayqiu'
+    })
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+
+    // data: { token: 'TVRBMk9qRTFPREl3TmpZNU5qRT0=' } }
+    // authenticating
+    axios.post(TODO_API_URL + '/auth', {
+        username: 'jayqiu'
+    })
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+
+    // creating new todo item under the currently authenticated user
+    // axios.post(TODO_API_URL + '/todo-item', {
+    //     content: 'first to-do'
+    // })
+    // .then(function(response) {
+    //     console.log(response);
+    // })
+    // .catch(function(error) {
+    //     console.log(error);
+    // });
+    
+    // get data about all todo items under the currently authorized user
+    // axios.get(TODO_API_URL + "/todo-item").then(function(response) {
+    //     res.render("user-todo", { list: response.data });
+    //   });
+
+});
+
 
 app.listen(3000);
