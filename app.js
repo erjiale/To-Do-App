55d3998bafa6b11438928c5d2019c194da42092e
erjiale <jiale98chinoguay@gmail.com>
2020-03-03 18:46:38 -0500

    completed the register, login, and logout functions

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -11,31 +12,37 @@ const app = express();
 app.engine("handlebars",exphbs());
 app.set("view engine", "handlebars");
 
+// reads in data.json
 var data = fs.readFileSync(path.join(__dirname, '/data.json'));
 var jsondata = JSON.parse(data);
-// console.log(jsondata);
+    // console.log(jsondata);
 
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(bodyParser.urlencoded({ extended: true })); // bodyParser allows for req.body.(...);
+app.use(cookieParser("secret")); // w/o this = Error: cookieParser("secret") required for signed cookies
+app.use(cookieParser()); // import cookie-parser middleware
 
 app.get('/', function(req, res) {
     // res.sendFile(path.join(__dirname + '/public/index.html'));
-    res.render("home", {datajson: jsondata})
+    res.render("home");
 });
 
 // part 1d - Submit Data to Express App Using HTML Form
-app.post("/", function(req, res) {
-    console.log("Data inputted: " + req.body.data_input);    
-    res.render("home");
-});
+// app.post("/", function(req, res) {
+//     console.log("Data inputted: " + req.body.data_input);    
+//     res.render("home");
+// });
 
 // part 1e - Use Node to Make HTTP Requests to the Hunter-to-do-app API
 TODO_API_URL = "https://hunter-todo-api.herokuapp.com";
 
-    // creates a new user with username: "jayqiu"
-app.get("/new_user", function(req,res) {
+app.get("/register", function(req,res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {   
     axios.post(TODO_API_URL + '/user', {
-        username: 'jayqiu'
+        username: req.body.username_input
     })
     .then(function(response) {
         console.log(response);
@@ -44,35 +51,133 @@ app.get("/new_user", function(req,res) {
         console.log(error);
     });
 
-    // data: { token: 'TVRBMk9qRTFPREl3TmpZNU5qRT0=' } }
-    // authenticating
-    axios.post(TODO_API_URL + '/auth', {
-        username: 'jayqiu'
-    })
-    .then(function(response) {
-        console.log(response);
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
+    res.render("home");
+});
 
-    // creating new todo item under the currently authenticated user
-    // axios.post(TODO_API_URL + '/todo-item', {
-    //     content: 'first to-do'
+app.get("/login", function(req,res) {
+    res.render("login");
+});
+
+app.post("/login", async (req,res) => {
+    // authenticatinG
+    try {
+        const response = await axios.post(TODO_API_URL + '/auth', { username: req.body.username_input});
+        res.cookie('Authentication', response.data.token, {
+            signed: true,
+            httpOnly: true
+        });
+        res.cookie('Username', req.body.username_input, {
+            signed: true,
+            httpOnly: true
+        });
+        res.redirect('/user');
+    } catch(err) {
+        if (err.response.status === 400) {
+            res.render("error", {
+                message: "User does not exist",
+                error: {status:400}
+            })
+        }
+        console.log(err);
+    }
+
+    app.get('/hi', (req,res) => {
+        res.send(req.signedCookies);
+    })
+
+
+    // var token = (await axios.post(TODO_API_URL + '/auth', {username: req.body.username_input, headers: {'content-type': application/json}}));
+    // res.cookie("Authentication", token);
+    // var todo = (await axios.get(TODO_API_URL + '/todo-item', {headers: {'Authorization': token} })).data;
+    // res.render('user', {username: req.body.username_input, list: todo}); 
+    // axios.post(TODO_API_URL + '/auth', {
+    //     username: req.body.username_input 
     // })
     // .then(function(response) {
-    //     console.log(response);
+    //     // console.log(response);
+    //     var token = response.data;
+    //     // res.cookie('Username', req.body.username_input, {
+    //     //     signed: true,
+    //     //     httpOnly: true
+    //     // });
+    //     res.cookie('Authentication', token, {
+    //         signed: true,
+    //         httpOnly: true
+    //     });
+    //     // res.render("user", {username:req.body.username_input});
+    //     // res.setHeader('Authentication', token);
+    //     res.render('user');
     // })
     // .catch(function(error) {
     //     console.log(error);
     // });
-    
-    // get data about all todo items under the currently authorized user
-    // axios.get(TODO_API_URL + "/todo-item").then(function(response) {
-    //     res.render("user-todo", { list: response.data });
-    //   });
-
 });
+
+app.get('/user', async (req,res) => {
+    // get data about all todo items under the currently authorized user
+    try {
+        if (req.signedCookies.Authentication === undefined) {
+            res.render('error', {
+                message: "User not authenticated",
+                error: {status:401}
+            })
+        }
+        else {
+            const response = await axios.get(TODO_API_URL + '/todo-item', {
+                headers: {
+                    Cookie: `token=${req.signedCookies.Authentication}`
+                }   
+            })
+            res.render('user', {username: req.body.username_input, list: response.data})
+        }
+    } catch(err) {
+        if (err.response.status === 400) {
+			res.render("error", {
+				message: "User doesn't exist",
+				error: {status: 400}
+			})
+		}
+        console.log(err);
+    }
+    // get data about all todo items under the currently authorized user
+    // var token = req.signedCookies.Authentication;
+    // axios.get(TODO_API_URL + "/todo-item", {
+    //     headers: {
+    //         'Authorization': token
+    //     }
+    // }).then(function(response) {
+    //     const todo = response.data;
+    //     res.render("user", { list: response.data });
+    // }).catch(function(error) {
+    //     console.log(error);
+    // });
+
+    // creating new todo item under the currently authenticated user
+    // axios.get(TODO_API_URL + '/todo-item', {
+    //     content: 'first to-do'
+    // })
+    // .then(function(response) {
+    //     console.log(response);
+    //     // res.render('user');
+    // })
+    // .catch(function(error) {
+    //     console.log(error);
+    // });
+
+    // res.render('user');
+});
+
+app.get('/logout', async (req,res)=> {
+    try {
+        res.clearCookie('Authentication');
+        res.clearCookie('Username');
+        res.redirect('/');
+        console.log("Successfully logged out")
+    } catch(err) {
+        console.log(err);
+    }
+});
+
 
 
 app.listen(3000);
